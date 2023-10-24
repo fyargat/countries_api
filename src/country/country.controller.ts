5548390effd65e9708f04602517b192cfb3b6cc6
Cass <bikbaevf18@gmail.com>
2023-10-24 18:47:20 +0400

    Update country controller

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -1,8 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 import { CountryService } from './country.service';
+import {
+  GetCountryResponse,
+  GetListRequest,
+  GetListResponse,
+} from './dto/country.dto';
 
 @Controller('country')
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+
+  @Get()
+  @ApiOkResponse({ type: GetListResponse })
+  async getList(
+    @Query(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+        forbidNonWhitelisted: true,
+      }),
+    )
+    query: GetListRequest,
+  ): Promise<GetListResponse> {
+    const result = await this.countryService.getList(query);
+    return result;
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: GetCountryResponse })
+  async getCountry(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<GetCountryResponse> {
+    const result = await this.countryService.getCountry(id);
+    return result;
+  }
 }
